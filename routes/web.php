c3820171ceb28690de030027d8c972bf11ebffc1
Callan Lamb <callan.lamb@gmail.com>
2017-01-06 08:30:21 -0500

    Bug fix: Upgrade register and login route syntax to reflect Laravel 5.3 file structure

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,14 @@ Route::get('/', function () {
 });
 
 // These functions come from the Illuminate\Foundation\Auth\AuthenticatesUsers package that's pulled into the LoginController method.
-Route::get('auth/login', 'Auth\LoginController@getLogin');
+Route::get('auth/login', 'Auth\LoginController@showLoginForm');
 
-Route::post('auth/login', 'Auth\LoginController@getLogin');
+Route::post('auth/login', 'Auth\LoginController@login');
 
-Route::get('auth/logout', 'Auth\LoginController@getLogout');
+Route::get('auth/logout', 'Auth\LoginController@logout');
 
 //Registration routes
 
-Route::get('auth/register', 'Auth\RegisterController@getRegister');
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
 
-Route::post('auth/register', 'Auth\RegisterController@postRegister');
+Route::post('auth/register', 'Auth\RegisterController@register');
