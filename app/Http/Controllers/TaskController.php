a418167dae2aee5a47fb06dfe451ad35f689651c
Callan Lamb <callan.lamb@gmail.com>
2017-01-07 17:05:57 -0500

    Add index function on TaskController with typehinting

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,4 +11,9 @@ class TaskController extends Controller
     {
     	$this->middleware('auth');
     }
+
+    public function index(Request $request)
+    {
+    	return view('tasks.index');
+    }
 }
