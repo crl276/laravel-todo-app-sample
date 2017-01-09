1d2784f6317a53ede9d1ed0905dfddf62b56ac36
Callan Lamb <callan.lamb@gmail.com>
2017-01-08 19:15:10 -0500

    Add redirect logic to destroy function on TaskController

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -40,5 +40,9 @@ class TaskController extends Controller
     public function destroy(Request $request, Task $task)
     {
         $this->authorize('destroy', $task);
+
+        $task->delete();
+
+        return redirect('/tasks');
     }
 }
