a6a9ce712f48ecc6ae12dd8c5e4d81c72c30fdfc
Callan Lamb <callan.lamb@gmail.com>
2017-01-08 21:40:12 -0500

    Update auth routes as per Laravel 5.2 documentation

--- routes/web.php
+++ routes/web.php
@@ -11,27 +11,13 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['middleware' => ['web']], function () {
+    Route::get('/', function () {
+        return view('welcome');
+    })->middleware('guest');
+    Route::get('/tasks', 'TaskController@index');
+    Route::post('/task', 'TaskController@store');
+    Route::delete('/task/{task}', 'TaskController@destroy');
+    Route::auth();
 });
-
-// These functions come from the Illuminate\Foundation\Auth\AuthenticatesUsers package that's pulled into the LoginController method.
-Route::get('auth/login', 'Auth\LoginController@showLoginForm');
-
-Route::post('auth/login', 'Auth\LoginController@login');
-
-Route::get('auth/logout', 'Auth\LoginController@logout');
-
-//Registration routes
-
-Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
-
-Route::post('auth/register', 'Auth\RegisterController@register');
-
-//Task Routes
-
-Route::get('/tasks', 'TaskController@index');
-
-Route::get('/task', 'TaskController@store');
-
-Route::delete('/task/{task}', 'TaskController@destroy');
